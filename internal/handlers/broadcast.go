a81f7f1a0479f53fdefcf0d7912faaaf2ca8c105
Peter Olsen <olse0321@umn.edu>
2026-04-07 18:17:42 -0500

    feat: add broadcast function

--- internal/handlers/broadcast.go
+++ internal/handlers/broadcast.go
@@ -1,9 +1,23 @@
 package handlers
 
 import (
+	"context"
+	"fmt"
+
+	"github.com/Cloud-RAMP/cloud-ramp.git/internal/redis"
 	wasmevents "github.com/Cloud-RAMP/wasm-sandbox/pkg/wasm-events"
 )
 
 func BroadcastHandler(event *wasmevents.WASMEventInfo) (string, error) {
-	return "dummy", nil
+	if len(event.Payload) < 1 {
+		return "", fmt.Errorf("No broadcast message specified")
+	}
+
+	return "", redis.Broadcast(
+		context.Background(),
+		event.InstanceId,
+		event.RoomId,
+		event.ConnectionId,
+		event.Payload[0],
+	)
 }
